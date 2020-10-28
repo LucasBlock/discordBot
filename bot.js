b71de58630f25fe999a3cd890bc75d3e9a498438
krugerleo <49465486+krugerleo@users.noreply.github.com>
2020-10-27 22:30:32 -0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -22,23 +22,26 @@ client.on('message', async msg => {
     if (!msg.content.startsWith(prefix)) return;
 
     // if === !audio ou !audio --help ou --commands
-    if(msg.content === "!audio" || msg.content === "!audio --help" || msg.content === "!audio --commands")
-        return msg.channel.send("To see the list of audios type '!audio --list', to play some audio type !audio NameOfAudio");
-        
+    if(msg.content === "!audio" || msg.content === "!audio --help" || msg.content === "!audio --commands"){
+        msg.channel.send("Para ver a lista de audios digite '!audio --lista', para reproduzir algum audio digite !audio NomeDoAudio");
+        return;
+    }
 
     const args = msg.content.split(" ");
 
-    if(args[1] === "--list"){
+    if(args[1] === "--lista"){
         fs.readdir(path, function (err, files) {
             //handling error
             if (err) {
                 return console.log('Unable to scan directory: ' + err);
             } 
             //listing all files using forEach
+            string = "";
             files.forEach(function (file) {
                 // Do whatever you want to do with the file
-                msg.channel.send(`${prefix} ${file.slice(0,-4)}`); 
+                string += `${file.slice(0,-4)}  ,`; 
             });
+            msg.channel.send(string)
         });
         return;
     }else{
@@ -49,7 +52,8 @@ client.on('message', async msg => {
             // The check succeeded
         } catch (error) {
             // The check failed
-            return msg.channel.send("No file match the given name"); 
+            msg.channel.send("Nenhum arquivo de audio coincide com esse comando"); 
+            return;
         }
     }
     // Only try to join the sender's voice channel if they are in one themselves
@@ -57,7 +61,7 @@ client.on('message', async msg => {
         const connection = await msg.member.voice.channel.join();
         const dispatcher = connection.play(`${path}${args[1]}.mp3`);
     } else {
-        return msg.reply('You need to be in a voice channel, darling!');
+        msg.reply('Você precisa estar em um canal de voz primeiramente, meu anjo!');
     }
 
     // If has the right permisssions
